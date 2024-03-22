c782dce308653ddbf017dd2b1ce212da917820e6
Creativementor <muhammadhamza318@gmail.com>
2024-03-23 04:21:20 +0500

    commit
    commit

--- script.js
+++ script.js
@@ -3,3 +3,28 @@
 var greeting = "Welcome to my website"
 
 alert(greeting)
+
+//2. Write a script to display following message on your web page:
+
+// var password = "Error! Please enter a valid password"
+
+// alert(password)
+
+
+// 3. Write a script to display following message on your web 
+//page: (Hint : Use line break)
+
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// 4. Write a script to display following messages in sequence:
+
+// alert("Welcome to JS Land")
+
+// alert('Happy Coding! \n  \n ✅ prevent this page from creating additional dialogs');
+
+// 5. Generate the following message through browser’s developer console
+
+// var sentence = "Hello... I can run JS through my web browser's console"
+
+// alert(sentence)
+
